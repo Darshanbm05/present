526976d005f0ecc1b0877cd3438d6257bdba9b88
Darshanbm05 <darshanjss71@gmail.com>
2026-02-18 22:07:23 +0530

    change in getplaces

--- backend/src/routes/busRoutes.js
+++ backend/src/routes/busRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getBuses} from "../controllers/busController.js";
+import {getBuses,getPlaces} from "../controllers/busController.js";
 
 const router=express.Router();
 
